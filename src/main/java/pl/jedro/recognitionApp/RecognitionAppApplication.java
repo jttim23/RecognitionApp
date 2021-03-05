59b49eeb9681d50b436554c55cf7a50257bf6ff5
jedro <jttim23@gmail.com>
2021-03-05 11:33:47 +0100

    initial commit

---
+++ src/main/java/pl/jedro/recognitionApp/RecognitionAppApplication.java
@@ -0,0 +1,13 @@
+package pl.jedro.recognitionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognitionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecognitionAppApplication.class, args);
+	}
+
+}
