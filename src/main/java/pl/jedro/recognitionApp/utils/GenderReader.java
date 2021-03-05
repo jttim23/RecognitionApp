8f86a89158af07b93f1dd180a9b47211d26aa4c0
jedro <jttim23@gmail.com>
2021-03-05 14:50:12 +0100

    refactored Gender Reader

--- src/main/java/pl/jedro/recognitionApp/utils/GenderReader.java
+++ src/main/java/pl/jedro/recognitionApp/utils/GenderReader.java
@@ -1,28 +1,24 @@
 package pl.jedro.recognitionApp.utils;
 
+import lombok.AllArgsConstructor;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+@AllArgsConstructor
 public class GenderReader {
+    private BufferedReader bufferedReader;
 
-    public GenderToken getGenderToken(FileReader fileReader) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String line = bufferedReader.readLine();
-        if (line == null) throw new IOException();
-        return new GenderToken(line);
-    }
-
-    public List<GenderToken> getAllTokens(FileReader fileReader) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+    public List<GenderToken> getAllTokens() throws IOException {
         List<GenderToken> list = new ArrayList<>();
         String line = bufferedReader.readLine();
         while (line != null) {
             list.add(new GenderToken(line));
             line = bufferedReader.readLine();
         }
+        if (list.isEmpty()) throw new IOException();
         return list;
     }
 }
--- src/test/java/pl/jedro/recognitionApp/utils/GenderReaderTests.java
+++ src/test/java/pl/jedro/recognitionApp/utils/GenderReaderTests.java
@@ -4,32 +4,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class GenderReaderTests {
-    private GenderReader reader;
     private FileReader femaleTokens;
     private FileReader emptyFemaleTokens;
+
     @BeforeEach
     void setUp() throws FileNotFoundException {
-         reader = new GenderReader();
-         femaleTokens = new FileReader( "src/main/resources/static/femaleTokens.txt");
-         emptyFemaleTokens = new FileReader( "src/main/resources/static/emptyFemaleTokens.txt");
+
+        femaleTokens = new FileReader("src/main/resources/static/femaleTokens.txt");
+        emptyFemaleTokens = new FileReader("src/main/resources/static/emptyFemaleTokens.txt");
+
     }
 
     @Test
     void shouldThrowIOExceptionWhenFileEmpty() {
-        Assertions.assertThrows(IOException.class, () -> reader.getGenderToken(emptyFemaleTokens));
+        Assertions.assertThrows(IOException.class, () -> new GenderReader(new BufferedReader(emptyFemaleTokens))
+                .getAllTokens());
     }
-    @Test
-    void shouldReturnAtLeastOneGenderToken() throws IOException {
-        Assertions.assertNotEquals(null,reader.getGenderToken(femaleTokens));
-    }
+
+
     @Test
     void shouldReturnAllGenderTokens() throws IOException {
-        Assertions.assertEquals(3, reader.getAllTokens(femaleTokens).size());
+        Assertions.assertEquals(3, new GenderReader(new BufferedReader(femaleTokens
+        )).getAllTokens().size());
     }
 }
